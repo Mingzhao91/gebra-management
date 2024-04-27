25b53faeaa2c591503e93efdb649c2dbe2f2af27
Mingzhao Zhou <edwardwhite7@hotmail.com>
2024-04-27 03:46:30 +0100

    format table

--- src/app/components/products-dashboard/products-dashboard.component.ts
+++ src/app/components/products-dashboard/products-dashboard.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Product } from '../../interfaces/product';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+  ],
   templateUrl: './products-dashboard.component.html',
-  styleUrl: './products-dashboard.component.scss'
+  styleUrl: './products-dashboard.component.scss',
 })
 export class ProductsDashboardComponent {
+  products!: Product[];
+  // table variables
+  displayedColumns: string[] = [
+    'pictureUrl',
+    'modelNumber',
+    'category',
+    'capacity',
+    'prices',
+    'description',
+  ];
+  dataSource!: MatTableDataSource<Product>;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private productService: ProductsService) {}
+
+  async ngOnInit() {
+    // Get products from server
+    this.products = await this.productService.getProducts();
+    // set products in table
+    this.dataSource = new MatTableDataSource(this.products);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
