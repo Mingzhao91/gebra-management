f17bf41907792444cf9d3ed8c273109d23275911
Mingzhao Zhou <edwardwhite7@hotmail.com>
2024-04-27 02:58:36 +0100

    init products dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import(
+        './components/products-dashboard/products-dashboard.component'
+      ).then((x) => x.ProductsDashboardComponent),
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
