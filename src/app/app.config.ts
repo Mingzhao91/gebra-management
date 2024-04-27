b80291e9791d7f3e8db0e9aa78bbb098fad022cc
Mingzhao Zhou <edwardwhite7@hotmail.com>
2024-04-27 01:57:19 +0100

    init project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"gebra-products-managemen-668af","appId":"1:474432050535:web:955cba7981897ff1ab4bc2","storageBucket":"gebra-products-managemen-668af.appspot.com","apiKey":"AIzaSyCInQDc7b2m1qdNmAIo1Xjvfx6OuxIn5CU","authDomain":"gebra-products-managemen-668af.firebaseapp.com","messagingSenderId":"474432050535","measurementId":"G-BKBVP14C45"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"gebra-products-managemen-668af","appId":"1:474432050535:web:11f214e082e6e2f5ab4bc2","storageBucket":"gebra-products-managemen-668af.appspot.com","apiKey":"AIzaSyCInQDc7b2m1qdNmAIo1Xjvfx6OuxIn5CU","authDomain":"gebra-products-managemen-668af.firebaseapp.com","messagingSenderId":"474432050535","measurementId":"G-B8P52W2H0K"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
