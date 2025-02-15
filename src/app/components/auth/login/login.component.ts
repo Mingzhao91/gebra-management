12a0c95af3ef320c09df38b007d827aa2fa21f45
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-02-15 17:29:08 +0000

    reactive form on login component

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import {
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
@@ -20,5 +28,23 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  onSubmit(form: NgForm) {}
+  constructor(private authService: AuthService) {}
+
+  loginForm!: FormGroup;
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+      }),
+      password: new FormControl('', { validators: [Validators.required] }),
+    });
+  }
+
+  onSubmit() {
+    this.authService.login({
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    });
+  }
 }
