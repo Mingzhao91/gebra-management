694748ea174651e5bb140bd8a0f1d2d2b7d0ac8c
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-02-15 15:18:27 +0000

    add navigation links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
@@ -17,6 +17,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     RouterOutlet,
     RouterModule,
     MatToolbarModule,
+    MatListModule,
     MatIconModule,
     MatTooltipModule,
     MatButtonModule,
