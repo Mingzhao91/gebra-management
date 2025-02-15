18b019c0864e38e4760356d22d006c9005ddd51a
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-02-15 17:30:27 +0000

    show and hide navigation items

--- src/app/components/navigation/header/header.component.ts
+++ src/app/components/navigation/header/header.component.ts
@@ -3,19 +3,39 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [RouterLink, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth!: boolean;
+  authSubscription!: Subscription;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+  }
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 }
--- src/app/components/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/components/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,21 +1,38 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../../../services/auth.service';
 @Component({
   selector: 'app-sidenav-list',
   standalone: true,
-  imports: [RouterModule, MatListModule, MatIconModule],
+  imports: [RouterLink, MatListModule, MatIconModule],
   templateUrl: './sidenav-list.component.html',
   styleUrl: './sidenav-list.component.scss',
 })
 export class SidenavListComponent {
   @Output() closeSidenav = new EventEmitter<void>();
+  isAuth!: boolean;
+  authSubscription!: Subscription;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => (this.isAuth = authStatus)
+    );
+  }
 
   onClose() {
     this.closeSidenav.emit();
+  }
+
+  ngOnDestro() {
+    this.authSubscription.unsubscribe();
   }
 }
