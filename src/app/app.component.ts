45d912634de8315cdffe41e34fc454606ba0fddc
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-02-15 16:00:53 +0000

    trigger navigation on side menu item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
@@ -14,7 +14,6 @@ import { SidenavListComponent } from './components/navigation/sidenav-list/siden
   imports: [
     CommonModule,
     RouterOutlet,
-    RouterModule,
     MatTooltipModule,
     MatButtonModule,
     MatSidenavModule,
--- src/app/components/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/components/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidenav-list',
   standalone: true,
-  imports: [MatListModule, MatIconModule],
+  imports: [RouterModule, MatListModule, MatIconModule],
   templateUrl: './sidenav-list.component.html',
   styleUrl: './sidenav-list.component.scss',
 })
