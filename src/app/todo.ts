8f8fa72c387941a3993598ec96af7d7e074dc9e2
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-02-16 09:14:11 +0000

    transfer todos in other file

---
+++ src/app/todo.ts
@@ -0,0 +1,4 @@
+// todo: create a firestore user when sign up, and then create a user with more details such as haveing roles in there https://www.youtube.com/watch?v=qWy9ylc3f9U
+// D - todo: setup privacy rule on each table, products: readonly if not login, else read & write if logged in
+// todo: implement canload for customer and order
+// todo: fetch with user id(customer is created by perticular user) and store document reference: https://chatgpt.com/c/67b1a8e1-7ddc-8007-b641-2b9d92c1247f
