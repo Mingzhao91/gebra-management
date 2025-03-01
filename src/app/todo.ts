f46bca25a412b86a3d8316b9bc47c9976135222e
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-03-01 18:03:34 +0000

    update todo

--- src/app/todo.ts
+++ src/app/todo.ts
@@ -1,12 +1,16 @@
 // website:
 // https://gebra-products-managemen-668af.web.app/
 
-// todo: create a firestore user when sign up, and then create a user with more details such as haveing roles in there https://www.youtube.com/watch?v=qWy9ylc3f9U
+// D - todo: create a firestore user when sign up, and then create a user with more details such as haveing roles in there https://www.youtube.com/watch?v=qWy9ylc3f9U
 // D - todo: setup privacy rule on each table, products: readonly if not login, else read & write if logged in
 // D - todo: implement canload for customer and order
 // D - todo: fetch with user id(customer is created by perticular user) and store document reference: https://chatgpt.com/c/67b1a8e1-7ddc-8007-b641-2b9d92c1247f
-// todo: cannot update anythings that doesn't belong to the creator
+// D - todo: cannot update anythings that doesn't belong to the creator
 // todo: translate into Chinese
 // D - todo: sign up to insert english name
-// todo: add decimal separator
-// todo: sort customer by first name
+// D - todo: add decimal separator
+// D -  todo: sort customer by first name in order dialog
+// todo: update the order records if customer get updated - better do it using firebase function:
+// exports.updateCustomerInProducts = functions.firestore
+//     .document("customers/{customerId}")
+//     .onUpdate(async (change, context) => {}
