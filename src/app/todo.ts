4dc4f7953052a60335f77d3a310b11f8fcf56cb7
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-03-01 14:44:59 +0000

    update todo list

--- src/app/todo.ts
+++ src/app/todo.ts
@@ -5,3 +5,8 @@
 // D - todo: setup privacy rule on each table, products: readonly if not login, else read & write if logged in
 // D - todo: implement canload for customer and order
 // D - todo: fetch with user id(customer is created by perticular user) and store document reference: https://chatgpt.com/c/67b1a8e1-7ddc-8007-b641-2b9d92c1247f
+// todo: cannot update anythings that doesn't belong to the creator
+// todo: translate into Chinese
+// D - todo: sign up to insert english name
+// todo: add decimal separator
+// todo: sort customer by first name
